4d425c2067c23333254e8024e99729ac313c9c30
Rain <smalltime153@gmail.com>
2021-09-13 11:11:18 +0800

    fix: :bug:

--- src/util.js
+++ src/util.js
@@ -1,6 +1,6 @@
 const { csv } = require('csvtojson/v2');
 
-exports.delay = (timeout) => new Promise((resolve) => setTimeout(() => resolve(), timeout));
+exports.delay = (timeout) => new Promise((resolve) => setTimeout(resolve, timeout));
 
 exports.csvToJson = async function csvToJson(csvStream) {
   return csv().fromStream(csvStream);
